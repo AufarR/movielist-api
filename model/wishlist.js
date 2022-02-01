7ccc303052607ea464148b5f627d8028fc898406
AufarR <aufaralazhar@gmail.com>
2022-02-01 18:58:35 +0700

    Cancelled wishlist.js change from 1b8c23c

--- model/wishlist.js
+++ model/wishlist.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const listSchema = new mongoose.Schema({
   movieId: { type: String},
-  user_id: { type: String }
+  userId: { type: String }
 });
 
 module.exports = mongoose.model("list", listSchema);
